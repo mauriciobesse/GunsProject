933a89a60f5f79fcc9ab328df35c9e0db395338a
mauriciobesse <cmbesse@gmail.com>
2014-05-14 10:18:08 -0300

    Marcas

---
+++ GunManagementSystem/ReglasNegocio/Marca.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReglasNegocio
+{
+    public class Marca
+    {
+        int MarcaID { set; get; }
+        string Nombre { set; get; }
+        bool Eliminada { set; get; }
+    }
+}
